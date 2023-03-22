07d9e0e07e42993fa00abab73ae310e0744c3e17
kels7 <hiketabo@gmail.com>
2023-03-22 22:29:08 +0000

    add logger and make async

--- calculations/extraction.py
+++ calculations/extraction.py
@@ -1,4 +1,7 @@
-def get_hash_states(all_states: dict, contract: str, variable: str) -> dict:
+from base_logger import logger
+
+
+async def get_hash_states(all_states: dict, contract: str, variable: str) -> dict:
     try:
         return all_states[contract][variable]
     except Exception as e:
